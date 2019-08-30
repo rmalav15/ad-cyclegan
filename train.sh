dd198d90a6dba76abd393040dee36238fd2f3e4c
Ram <rmalav15@gmail.com>
2019-08-30 22:51:40 +0530

    unet concat noise training

--- train.sh
+++ train.sh
@@ -2,4 +2,5 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --dataset_dir /mnt/069A453E9A452B8D/Ram/adGeneration/cyclegan \
     --input_nc 5 \
-    --batch_size 4
+    --batch_size 4 \
+    --epoch 500
